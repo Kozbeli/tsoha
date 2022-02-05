d78127faf87a8d16f7521d9c1c4f4cf941e25b18
Kozbeli <ossi.ilmanen@gmail.com>
2022-02-05 18:56:21 +0200

    add reset database

--- schema/createdb.sql
+++ schema/createdb.sql
@@ -1,5 +1,31 @@
-INSERT INTO users (username, password, email, admin) VALUES
-    ('asdf',
-     'pbkdf2:sha256:260000$PhvkK5Ec3d7CtEkR$e2f94bfe8c974af1b8169d1730782eafc0017e16dd41df48f665b39e8ce95db9',
-     'asdf@asdf.com',
-     True);
+INSERT INTO users (username, password, email)
+VALUES ('asdf',
+        'pbkdf2:sha256:260000$PhvkK5Ec3d7CtEkR$e2f94bfe8c974af1b8169d1730782eafc0017e16dd41df48f665b39e8ce95db9',
+        'asdf@asdf.com');
+
+INSERT INTO users (username, password, email)
+VALUES ('hjkl',
+        'pbkdf2:sha256:260000$PhvkK5Ec3d7CtEkR$e2f94bfe8c974af1b8169d1730782eafc0017e16dd41df48f665b39e8ce95db9',
+        'hjkl@hjkl.com');
+
+INSERT INTO vehicles (reg_nro, manufacturer, model, capacity)
+VALUES ('asd-123',
+        'Toyota',
+        'Corolla',
+        5);
+
+INSERT INTO vehicle_owners (user_id, vehicle_id) VALUES (1,1);
+
+INSERT INTO trips (vehicle_id, user_id, departure, destination, depart_time)
+VALUES (1,
+        1,
+        'Helsinki',
+        'Tampere',
+        '1111-11-11 07:00:00');
+
+INSERT INTO trips (vehicle_id, user_id, departure, destination, depart_time)
+VALUES (1,
+        1,
+        'Tampere',
+        'Helsinki',
+        '1111-11-11 17:00:00');
--- schema/schema.sql
+++ schema/schema.sql
@@ -1,32 +1,42 @@
-CREATE TABLE IF NOt EXISTS visitors(
-  id SERIAL PRIMARY KEY, 
-  time TIMESTAMP
+CREATE TABLE IF NOT EXISTS visitors(
+    id SERIAL PRIMARY KEY,
+    time TIMESTAMP
 );
 
-CREATE TABLE IF NOt EXISTS users(
-  id SERIAL PRIMARY KEY,
-  username TEXT UNIQUE,
-  password TEXT,
-  email TEXT,
-  admin BOOLEAN
+CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    email TEXT
 );
 
-CREATE TABLE IF NOt EXISTS vehicles(
+CREATE TABLE IF NOT EXISTS vehicles(
     id SERIAL PRIMARY KEY,
     reg_nro TEXT,
     manufacturer TEXT,
-    model TEXT
+    model TEXT,
+    capacity INTEGER
 );
 
-CREATE TABLE IF NOt EXISTS vehicle_owners(
+CREATE TABLE IF NOT EXISTS vehicle_owners(
     user_id INTEGER REFERENCES users,
     vehicle_id INTEGER REFERENCES vehicles
 );
 
-CREATE TABLE IF NOt EXISTS messages(
-  id SERIAL PRIMARY KEY,
-  title TEXT,
-  content TEXT,
-  user_id INTEGER REFERENCES users,
-  sent_at TIMESTAMP
+CREATE TABLE IF NOT EXISTS messages(
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    content TEXT,
+    user_id INTEGER REFERENCES users,
+    sent_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS trips(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    vehicle_id INTEGER REFERENCES vehicles,
+    departure TEXT,
+    destination TEXT,
+    depart_time TIMESTAMP,
+    created_at TIMESTAMP
 );
